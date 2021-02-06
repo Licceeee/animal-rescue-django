eb530372cd99e3ecdf1a1107fdb7240084c3e66c
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-06 16:57:56 +0100

    animal images added to db + preview in core libs

--- project/animal/models.py
+++ project/animal/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy, gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from uuid import uuid4
 
-from core.libs.core_libs import (get_headshot_image, get_image_format)
+from core.libs.core_libs import (get_headshot_image, get_image_format)  # noqa
 
 
-# ----------------------------------------------------------------- >> FUNCTIONS
+# ---------------------------------------------------------------- >> FUNCTIONS
 def img_dir_path(instance, filename,):
     ext = filename.split('.')[-1]
     if instance.pk:
@@ -16,9 +16,9 @@ def img_dir_path(instance, filename,):
     return (f'animals/{filename}')
 
 
-# ------------------------------------------------------------------- >> CLASSES
+# ------------------------------------------------------------------ >> CLASSES
 class AnimalType(models.Model):
-    """model for type of animal. 
+    """model for _type of animal.
         e.g.: cat, dog, mouse, etc..
     """
     name = models.CharField(max_length=256, unique=True)
@@ -30,10 +30,11 @@ class AnimalType(models.Model):
 
 
 class AnimalCondition(models.Model):
-    """model for condition of animal. 
+    """model for condition of animal.
         e.g.: injured, ill, malnourished, good shape, etc..
     """
     name = models.CharField(max_length=256, unique=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -42,15 +43,15 @@ class AnimalCondition(models.Model):
 
 
 class Animal(models.Model):
-    """model for condition of animal. 
+    """model for condition of animal.
         e.g.: injured, ill, malnourished, good shape, etc..
     """
-    type = models.ForeignKey(AnimalType, on_delete=models.PROTECT)
+    _type = models.ForeignKey(AnimalType, on_delete=models.PROTECT)
     conditions = models.ManyToManyField(AnimalCondition)
+    description = models.CharField(max_length=256, null=True, blank=True)
     name = models.CharField(max_length=256, null=True, blank=True)
     image = models.ImageField(default=None, upload_to=img_dir_path,
                               null=True, blank=True)
-
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -58,7 +59,7 @@ class Animal(models.Model):
     def __str__(self):
         conditions = ", ".join([
             condition.name for condition in self.conditions.all()])
-        return f"{self.type.name}: {conditions}"
+        return f"{self._type.name}: {conditions}"
 
     def get_conditions(self):
         return ", ".join([
@@ -81,6 +82,29 @@ class Animal(models.Model):
     get_image.short_description = _('Image')
 
 
+class AnimalImage(models.Model):
+    animal = models.ForeignKey(Animal, default=None,
+                               on_delete=models.CASCADE)
+    image = models.ImageField(default=None, upload_to=img_dir_path,
+                              null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return (f"{self.animal._type.name}: {self.animal.name}")
 
+    def get_image(self):
+        return get_image_format(self.image)
 
+    get_image.short_description = _('Image')
+
+    def headshot_image(self):
+        return get_headshot_image(self.image)
+    headshot_image.short_description = _('Preview')
+
+    def get_animal(self):
+        return (f"{self.animal._type.name}: {self.animal.name}")
+    get_animal.short_description = _('Animal')
+
+    def get_post_date(self):
+        return self.animal.created
+    get_post_date.short_description = _('Date')
