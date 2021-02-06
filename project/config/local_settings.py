d7c965477617038fba7ccd049bf6709eeabf8e08
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-06 22:15:12 +0100

    custom user model added

--- project/config/local_settings.py
+++ project/config/local_settings.py
@@ -1,1 +1,8 @@
 SECRET_KEY = '1#t5@#9y20%vi5mx6ks8e#cl1q5#%=rt_=8nvk!*byjct(ef__'
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'schonefeld.dev@gmail.com'
+EMAIL_HOST_PASSWORD = 'sedmkyfypshevgfs'
+EMAIL_USE_TLS = True
