c996d20e75dacd013e4d9355c731c43abdc9ade9
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-12 21:59:14 +0100

    error urls/view/templates added

--- project/config/settings.py
+++ project/config/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = 'prod-key-placeholder'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
