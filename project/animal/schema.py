0ffd39b4272d68e1718d04f3fb102b431985d9e5
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-13 23:25:30 +0100

    graphene schema querys and mutation added

--- project/animal/schema.py
+++ project/animal/schema.py
@@ -1,18 +1,31 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 
-from .models import Animal, AnimalType, AnimalCondition
+from .models import Animal, AnimalType, AnimalCondition, AnimalGroup
 
 
 # ============================================================= >> ANIMAL NODES
 class AnimalNode(DjangoObjectType):
     class Meta:
+        description = "Representation of an animal"
         model = Animal
+
+
+class AnimalGroupNode(DjangoObjectType):
+    class Meta:
+        description = "Representation of an animal group"
+        model = AnimalGroup
 
 
 class AnimalTypeNode(DjangoObjectType):
     class Meta:
         model = AnimalType
+        only_fields = [
+            "name",
+            "icon",
+            'animal_set',
+            'get_animal_numbers'
+        ]
 
 
 class AnimalConditionNode(DjangoObjectType):
@@ -27,7 +40,31 @@ class CreateAnimalCondition(graphene.Mutation):
     class Arguments:
         name = graphene.String()
 
-    def mutate(self, info, name):
-        cond = AnimalCondition(name=name)
+    def mutate(self, info, **data):
+        cond = AnimalCondition(**data)
         cond.save()
         return CreateAnimalCondition(condition=cond)
+
+
+class CreateAnimal(graphene.Mutation):
+    animal = graphene.Field(AnimalNode)
+
+    class Arguments:
+        name = graphene.String()
+        is_chipped = graphene.Boolean()
+        chip = graphene.String()
+        age_years = graphene.Int()
+        age_months = graphene.Int()
+        neutered = graphene.Boolean()
+        # location
+        # _type
+        # post_type
+        # conditions
+        # image
+        # gender
+
+    @classmethod
+    def mutate(self, info, **data):
+        a = Animal(**data)
+        a.save()
+        return CreateAnimal(animal=a)
--- project/config/schema.py
+++ project/config/schema.py
@@ -1,17 +1,20 @@
 import graphene
-from graphene_django.types import DjangoObjectType
 
-from animal.models import Animal, AnimalType, AnimalCondition
-from animal.schema import (AnimalNode, AnimalTypeNode, AnimalConditionNode,
-                           CreateAnimalCondition)
+from animal.models import (Animal, AnimalGroup, AnimalType, AnimalCondition)
+from animal.schema import (AnimalNode, AnimalGroupNode, AnimalTypeNode,
+                           AnimalConditionNode)
+from animal.schema import (CreateAnimalCondition, CreateAnimal)
 
 from user.models import CustomUser
 from user.schema import UserNode, CreateUser
+
+# https://django-graphql-jwt.domake.io/en/latest/quickstart.html#installation
 
 
 class Query(graphene.ObjectType):
     animals = graphene.List(AnimalNode)
     animal_types = graphene.List(AnimalTypeNode)
+    animal_groups = graphene.List(AnimalGroupNode)
     animal_conditions = graphene.List(AnimalConditionNode)
 
     users = graphene.List(UserNode)
@@ -22,7 +25,10 @@ class Query(graphene.ObjectType):
     def resolve_animal_types(self, info, **kwargs):
         return AnimalType.objects.all()
 
-    def resolve_animal_condistions(self, info, **kwargs):
+    def resolve_animal_groups(self, info, **kwargs):
+        return AnimalGroup.objects.all()
+
+    def resolve_animal_conditions(self, info, **kwargs):
         return AnimalCondition.objects.all()
 
     def resolve_users(self, info, **kwargs):
@@ -30,6 +36,7 @@ class Query(graphene.ObjectType):
 
 
 class Mutation(graphene.ObjectType):
+    create_animal = CreateAnimal.Field()
     create_condition = CreateAnimalCondition.Field()
     create_user = CreateUser.Field()
 
@@ -37,18 +44,24 @@ class Mutation(graphene.ObjectType):
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
 
-# """
-#     query {
-#         animals {
-#             id
+# query {
+#     animals {
+#         id
+#         name
+#         image
+#         Type {
 #             name
-#             image
-#             Type {
-#                 name
-#             }
-#             conditions {
-#                 name
-#             }
+#         }
+#         conditions {
+#             name
 #         }
 #     }
-# """
+# }
+# mutation {
+#     createCondition(name: "freak") {
+#         condition {
+#             id
+#             name
+#         }
+#     }
+# }
